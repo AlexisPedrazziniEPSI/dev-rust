7559d0079a1c70e4da6d45097d6ef7897123484a
AlexisPedrazziniEPSI <145668867+AlexisPedrazziniEPSI@users.noreply.github.com>
2024-09-23 16:25:03 +0200

    First commit

---
+++ app_launcher/target/debug/deps/app_launcher-60f30b93521e59ae.d
@@ -0,0 +1,5 @@
+C:\Users\alexi\Documents\dev rust\app_launcher\target\debug\deps\libapp_launcher-60f30b93521e59ae.rmeta: src/main.rs
+
+C:\Users\alexi\Documents\dev rust\app_launcher\target\debug\deps\app_launcher-60f30b93521e59ae.d: src/main.rs
+
+src/main.rs:
---
+++ app_launcher/target/debug/deps/app_launcher-de4ff6d1065401f8.d
@@ -0,0 +1,5 @@
+C:\Users\alexi\Documents\dev rust\app_launcher\target\debug\deps\libapp_launcher-de4ff6d1065401f8.rmeta: src/main.rs
+
+C:\Users\alexi\Documents\dev rust\app_launcher\target\debug\deps\app_launcher-de4ff6d1065401f8.d: src/main.rs
+
+src/main.rs:
